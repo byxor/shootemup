b12f44011eef11303765eb64a1d857e9a131a6b6
byxor <brandnewbyxor@gmail.com>
2019-01-04 07:34:58 +0000

    Add more projectile tests

--- src/test/kotlin/AProjectile.kt
+++ src/test/kotlin/AProjectile.kt
@@ -6,11 +6,11 @@ class AProjectile {
 
     @Test
     fun mustHaveConsistentDimensions() {
-        data class D(val position: Vector, val velocity: Vector)
+        data class T(val position: Vector, val velocity: Vector)
         listOf(
-                D(listOf(0),          listOf(0, 0)),
-                D(listOf(0, 0),       listOf(0)),
-                D(listOf(1, 2, 3, 4), listOf(9, 8, 7, 6, 5))
+                T(listOf(0),          listOf(0, 0)),
+                T(listOf(0, 0),       listOf(0)),
+                T(listOf(1, 2, 3, 4), listOf(9, 8, 7, 6, 5))
         ).forEach({
             try {
                 Projectile(it.position, it.velocity)
@@ -23,13 +23,12 @@ class AProjectile {
 
     @Test
     fun willNotMoveWhenItsVelocityIsZero() {
-        val positions = listOf(
+        listOf(
                 listOf(0),
                 listOf(1),
                 listOf(0, 0),
                 listOf(1, 2, 3)
-        )
-        positions.forEach({ position ->
+        ).forEach({ position ->
             val velocity = MutableList(position.size) {0}
             val projectile = Projectile(position, velocity)
             val next = projectile.travel()
@@ -39,16 +38,27 @@ class AProjectile {
 
     @Test
     fun willTravelToTheExpectedPosition() {
-        data class D(val velocity: Vector,
+        data class T(val velocity: Vector,
                      val expectedFirst: Vector,
                      val expectedSecond: Vector)
 
         listOf(
-                D(listOf(1), listOf(1), listOf(2)),
-                D(listOf(10), listOf(10), listOf(20)),
-                D(listOf(15), listOf(15), listOf(30)),
-                D(listOf(-1), listOf(-1), listOf(-2)),
-                D(listOf(-1000), listOf(-1000), listOf(-2000))
+                // 1-Dimensional
+                T(listOf(1),     listOf(1),     listOf(2)),
+                T(listOf(10),    listOf(10),    listOf(20)),
+                T(listOf(15),    listOf(15),    listOf(30)),
+                T(listOf(-1),    listOf(-1),    listOf(-2)),
+                T(listOf(-1000), listOf(-1000), listOf(-2000)),
+
+                // 2-Dimensional
+                T(listOf(1, 1),  listOf(1, 1),  listOf(2, 2)),
+                T(listOf(0, 1),  listOf(0, 1),  listOf(0, 2)),
+                T(listOf(0, 10), listOf(0, 10), listOf(0, 20)),
+                T(listOf(-5, 1), listOf(-5, 1), listOf(-10, 2)),
+
+                // 3-Dimensional
+                T(listOf(1, 1, 1),    listOf(1, 1, 1),    listOf(2, 2, 2)),
+                T(listOf(90, -15, 3), listOf(90, -15, 3), listOf(180, -30, 6))
         ).forEach({
             val position = MutableList(it.velocity.size) {0}
             val projectile = Projectile(position, it.velocity)
