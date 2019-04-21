feebdae81c698623e4a01006dc9610c185d50c9e
byxor <brandnewbyxor@gmail.com>
2019-04-21 17:31:49 +0100

    Refactor

--- src/main/kotlin/shootemup/collision/PointVsCircle.kt
+++ src/main/kotlin/shootemup/collision/PointVsCircle.kt
@@ -4,10 +4,14 @@ import kotlin.math.abs
 
 fun isColliding(c1: PointCollider, c2: CircleCollider): Boolean {
     // https://www.mathsisfun.com/algebra/distance-2-points.html
-    // Square root isn't required for distance comparison.
+    //
+    // We don't need to calculate a square root to compare distances.
+    // It's faster to square the other number before comparing.
+
     val distanceSquared = (c2.position zip c1.position)
             .map { (a, b) -> (b - a).squared }
             .reduce { sum, component ->  sum + component }
+
     return distanceSquared < c2.radius.squared
 }
 
